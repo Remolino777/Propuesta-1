2d31f4362fa04f9a06a1e6274c42e3d38bc38c35
Javier <7.matute@gmail.com>
2024-04-04 16:25:29 -0500

    App ready

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ import time
 import joblib
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import toml
 
-#______________________________________________LOAD FILE
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-    
 
-#______________________________________________CACHE ELEMENTS
+
+
+
+
+
+
+#______________________________________________CACHE ELEMENTS FUNCTIONS
+
+
+
 @st.cache_resource
 def cargar_imagen():    
     return "psa logo.png"
@@ -26,7 +32,6 @@ def load_ml():
 def load_sc():    
     sc = joblib.load('scaler.pkl')
     return  sc
-
 
 
 classifier = load_ml()
@@ -51,198 +56,160 @@ pr_asccc = 0
 rPSA_entre_7_8 = 0
 rPSA_mayor_a_10 = 0
 
-
-#______________________________________________FUNCIONES
-
-
-
-
-#______________________________________________AUTENTICATOR
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],    
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
-)
-
-#Render de loggin widget
-fields_01 = ['login', 'main']
-name, authentication_status, username = authenticator.login(fields=fields_01)
-
-
-
-
-        
-if st.session_state["authentication_status"]:
+#________________________________________________REGISTER FORM 
+with st.form('registro',  clear_on_submit=True):
     
-    authenticator.logout()
-    with st.spinner('Wait for it...'): # Pantalla de carga
-        time.sleep(2)
-    st.write(f'Welcome *{st.session_state["name"]}*')    
+    st.title('Medical record')
+    st.subheader("Patient's data:")      
     
-    with st.form('registro', clear_on_submit=True):
+    
+    nombre, apellido, edad, id = st.columns([2,2,1,3])
+    
+    with nombre:
+        st.text_input('Name', placeholder='Name')
         
-        st.title('Registro medico')
-        st.subheader('Datos del paciente:')      
+    with apellido:
+        st.text_input('Lastname', placeholder='Lastname')
         
+    with edad:    
+        edad = st.number_input('Age', min_value=0, placeholder='Age', step=1)
         
-        nombre, apellido, edad, id = st.columns([2,2,1,3])
+    with id:
+        st.number_input('Id', placeholder='Id number', step=1)
+    
+    st.divider()  # ________________________________________________________    
+    st.subheader('Prostate specific antigen (PSA):')  
+    
+    psa = st.radio('',
+                ['PSA 0 - 3.99', 'PSA 4 - 6.99', 'PSA 7 - 7.99', 'PSA 8 - 9.99', 'PSA 10+'],
+                index=None, horizontal=True)        
+    
+    st.divider()  # ________________________________________________________
+    
+    tDiasPB, nDias = st.columns([3,1])
+    
+    with tDiasPB:
+        st.write('Number of days post biopsy in which the infectius complication occurs:')
         
-        with nombre:
-            st.text_input('Nombre', placeholder='Nombre')
-            
-        with apellido:
-            st.text_input('Apellido', placeholder='Apellido')
-            
-        with edad:    
-            edad = st.number_input('Edad', min_value=0, placeholder='Edad', step=1)
-            
-        with id:
-            st.number_input('Id', placeholder='Numero de Identificacion', step=1)
+    with nDias:
+        ndpb =   st.number_input('', min_value=0, step=1) 
         
-        st.divider()  # ________________________________________________________    
-        st.subheader('Niveles Antigeno Prostatico (PSA):')  
+    st.divider()  # ________________________________________________________
         
-        psa = st.radio('',
-                 ['PSA 0 - 3.99', 'PSA 4 - 6.99', 'PSA 7 - 7.99', 'PSA 8 - 9.99', 'PSA 10+'],
-                 index=None, horizontal=True)        
-        
-        st.divider()  # ________________________________________________________
-        
-        tDiasPB, nDias = st.columns([3,1])
-        
-        with tDiasPB:
-            st.write('NUMERO DE DIAS POST BIOPSIA EN QUE SE PRESENTA LA COMPLICACIÓN INFECCIOSA')
-            
-        with nDias:
-            ndpb =   st.number_input('', min_value=0, step=1) 
-            
-        st.divider()  # ________________________________________________________
-            
-                   
-        
-        # Positivo para 
-        st.subheader('Positivo para :')
-        hos, fi, diabetes, v_pros= st.columns(4)
-        
-        with hos:
-            hos = st.checkbox('Hospitalizacion ultimo mes')
-        with fi:
-            fiebre = st.checkbox('Fiebre')
-              
-        with diabetes:
-            diabetes = st.checkbox('Diabetes')  
-        with v_pros:            
-            v_pros = st.checkbox('Volumen prostatico')
-            
-        ecp, cup, b_p, itu1 = st.columns(4)
                 
-        with ecp:
-            ecp = st.checkbox('Enfermedad cronica pulmonar')
-        with cup:
-            cup = st.checkbox('CUP')
-        with b_p:
-            biopsia_previa = st.checkbox('Biopsias previas')
-                
-        with itu1:
-            itu7 = st.checkbox('ITU')            
-            
-        st.subheader('resultados laboratorio :')
-        
-        TC= st.selectbox('Tipo de cultivo :', ('HEMOCULTIVO Y UROCULTIVO', 'UROCULTIVO'), index=None, placeholder='Seleccione un patrón de resistencia......')
-        
-        
-        PR = st.selectbox('Patron de resistencia', ('AMPI, CIPRO Y GENTA', 'AMPI, SULFA, CEFADROXILO, CEFUROXIMO, CIPRO Y CEFEPIME, CEFOTAXIMA'), index=None, placeholder='Seleccione un patrón de resistencia......')
-        
-        
-        AA = st.selectbox('Agente aislado', ('No', 'E.Coli', 'Psudomonas aeruginosa'), index=None, placeholder='Seleccione un agente aislado......')
-        
-        Biopsia = st.selectbox('Resultado biopsia', ('Negativa','Adenocarcinoma Gleason 6',
-                               'Adenocarcinoma Gleason 7',
-                               'Adenocarcinoma Gleason 8',
-                               'Adenocarcinoma Gleason 9',
-                               'Carcinoma indiferenciado de celulas claras',
-                               'Prostatitis',
-                               'Hiperplasica prostatica'                               
-                               ),index=None, placeholder='Seleccione un resultado......' )
-        
-        ATP = st.selectbox('Antibiotico utilizado en la profilaxis',('Fluoroquinolona aminoglicosido', 
-                           'Oroquinolona', 
-                           'Cefalosporina aminoglucocido',
-                           'Otros',
-                           ),index=None, placeholder='Seleccione un antibiotico......')
-        
-        
-        st.divider()#_______________________________________________
-        
-        
-        
-        s_button = st.form_submit_button(label='Registrar')
-        
-        
-        
-        if s_button:  # ____________________________________________ FORM LOGIC
-            
-            if itu7:
-                itu_si = 1                
-            if fiebre:
-                fiebre_si = 1
-            if biopsia_previa:
-                biopsia_neg = 1 
-            if psa == 'PSA 7 - 7.99':
-                rPSA_entre_7_8 = 1            
-            if psa == 'PSA 10+':                
-                rPSA_mayor_a_10 = 1
-            # Patron de resistencia    
-            if PR == 'AMPI, CIPRO Y GENTA':
-                pr_acg = 1
-            if PR == 'AMPI, SULFA, CEFADROXILO, CEFUROXIMO, CIPRO Y CEFEPIME, CEFOTAXIMA':
-                pr_asccc = 1
-            if AA == 'E.Coli':
-                aa_ecoli = 1
-            # Tipo de cultivos
-            if TC == 'HEMOCULTIVO Y UROCULTIVO':
-                tc_hyu = 1                
-            if TC == 'UROCULTIVO':
-                tc_u = 1
-            
-            v_clinicas = [ndpb, biopsia_neg, fiebre_si, itu_si,  tc_hyu,
-              tc_u, aa_ecoli, pr_acg, pr_asccc, rPSA_entre_7_8, rPSA_mayor_a_10
-                        ]
-            # v_clinicas_2d = np.array(v_clinicas).reshape(1,-1)
-            
-            # prediccion de modelo            
-            
-            hospitalizacion = classifier.predict(sc.transform([v_clinicas])) 
-            
-            with st.spinner('Procesando los datos...'):
-                time.sleep(2)
-            
-            if hospitalizacion == 1:
-                st.warning('El pasiente nesecita ser hospitalizado', icon="⚠️")
-            else:
-                st.write('Paciente registrado')
     
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
+    # Positivo para 
+    st.subheader('Positive for:')
+    hos, fi, diabetes, v_pros= st.columns(4)
+    
+    with hos:
+        hos = st.checkbox('Hospitalization last month')
+    with fi:
+        fiebre = st.checkbox('Fever')
+            
+    with diabetes:
+        diabetes = st.checkbox('Diabetes')  
+    with v_pros:            
+        v_pros = st.checkbox('Prostate volume')
+        
+    ecp, cup, b_p, itu1 = st.columns(4)
+            
+    with ecp:
+        ecp = st.checkbox('Chronic lung disease')
+    with cup:
+        cup = st.checkbox('CUP')
+    with b_p:
+        biopsia_previa = st.checkbox('Previous biopsies')
+            
+    with itu1:
+        itu7 = st.checkbox('ITU')            
+        
+    st.subheader('Lab results :')
+    
+    TC= st.selectbox('Tipo de cultivo :', ('HEMOCULTIVO Y UROCULTIVO', 'UROCULTIVO'), index=None, placeholder='Select a resistance pattern......')
     
     
-elif st.session_state["authentication_status"] is None:
-    logout_completed = True
-    with st.spinner('Wait for it...'): # Pantalla de carga
-        time.sleep(3)
+    PR = st.selectbox('Resistance pattern', ('AMPI, CIPRO Y GENTA', 'AMPI, SULFA, CEFADROXILO, CEFUROXIMO, CIPRO Y CEFEPIME, CEFOTAXIMA'), index=None, placeholder='Select a resistance pattern......')
+    
+    
+    AA = st.selectbox('Isolated agent', ('No', 'E.Coli', 'Psudomonas aeruginosa'), index=None, placeholder='Select an isolated agent......')
+    
+    Biopsia = st.selectbox('Biopsy result', ('Negativa','Adenocarcinoma Gleason 6',
+                            'Adenocarcinoma Gleason 7',
+                            'Adenocarcinoma Gleason 8',
+                            'Adenocarcinoma Gleason 9',
+                            'Carcinoma indiferenciado de celulas claras',
+                            'Prostatitis',
+                            'Hiperplasica prostatica'                               
+                            ),index=None, placeholder='Test result......' )
+    
+    ATP = st.selectbox('Antibiotic used in prophylaxis',('Fluoroquinolona aminoglicosido', 
+                        'Oroquinolona', 
+                        'Cefalosporina aminoglucocido',
+                        'Otros',
+                        ),index=None, placeholder='Antibiotic sellect......')
+    
+    
+    st.divider()#_______________________________________________
+    
+    
+    
+    s_button = st.form_submit_button(label="Save patient's data")
+    
+    
+    
+    if s_button:  # ____________________________________________ FORM LOGIC
         
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        pass  
-    with col2:   
-        st.write("")
-        st.image(ruta_img)
-        st.write("")        
-    with col3:
-        pass
-    st.subheader("LABORATORIO EXPERIMENTAL DEL CANCER DE PROSTATA")     
+        if itu7:
+            itu_si = 1                
+        if fiebre:
+            fiebre_si = 1
+        if biopsia_previa:
+            biopsia_neg = 1 
+        if psa == 'PSA 7 - 7.99':
+            rPSA_entre_7_8 = 1            
+        if psa == 'PSA 10+':                
+            rPSA_mayor_a_10 = 1
+        # Patron de resistencia    
+        if PR == 'AMPI, CIPRO Y GENTA':
+            pr_acg = 1
+        if PR == 'AMPI, SULFA, CEFADROXILO, CEFUROXIMO, CIPRO Y CEFEPIME, CEFOTAXIMA':
+            pr_asccc = 1
+        if AA == 'E.Coli':
+            aa_ecoli = 1
+        # Tipo de cultivos
+        if TC == 'HEMOCULTIVO Y UROCULTIVO':
+            tc_hyu = 1                
+        if TC == 'UROCULTIVO':
+            tc_u = 1
+        
+        v_clinicas = [ndpb, biopsia_neg, fiebre_si, itu_si,  tc_hyu,
+            tc_u, aa_ecoli, pr_acg, pr_asccc, rPSA_entre_7_8, rPSA_mayor_a_10
+                    ]
+        # v_clinicas_2d = np.array(v_clinicas).reshape(1,-1)
+        
+        # prediccion de modelo            
+        
+        hospitalizacion = classifier.predict(sc.transform([v_clinicas])) 
+        
+        with st.spinner('Procesando los datos...'):
+            time.sleep(2)
+        
+        if hospitalizacion == 1:
+            st.warning(' The patient needs to be hospitalized ', icon="⚠️")
+        else:
+            st.write('Patient data saved')
+
+#________________________________________________LATERAL BAR
 
 
 
 
+
+# Si el usuario marca la opción para mostrar la barra lateral
+with st.sidebar:
+    st.subheader('INFO')
+    st.image(cargar_imagen())
+    st.write("To register the patient in the clinic's database, please complete all the corresponding fields in the form.")
+    st.divider()
+    st.write('PSA 2024 - @r3mo')
---
+++ readme.txt
@@ -0,0 +1,41 @@
+Patient Record Form Application
+This application allows users to record patient data in a medical clinic and predict whether the patients need to be hospitalized based on the provided information.
+
+Features
+User-friendly Interface: The application provides a simple and intuitive form for entering patient data.
+Predictive Functionality: Using machine learning models, the application can predict whether a patient needs to be hospitalized based on the recorded data.
+Scalable: Easily scalable to accommodate additional features or data inputs in the future.
+Secure: Patient data is securely stored and can only be accessed by authorized personnel.
+How to Use
+Fill Out the Form: Enter the required patient information in the provided form fields. This includes basic demographic information, medical history, laboratory results, and more.
+
+Submit the Form: Once all required fields are filled out, click the "Submit" button to record the patient data.
+
+View Prediction: After submitting the form, the application will process the data and provide a prediction regarding whether the patient needs to be hospitalized.
+
+Technologies Used
+Streamlit: Used to build the user interface and handle form submissions.
+Python: Backend language used for data processing and machine learning model implementation.
+Scikit-learn: Machine learning library used for predictive modeling.
+Pandas: Data manipulation library used for handling structured data.
+Installation
+Clone the repository to your local machine:
+
+bash
+Copy code
+git clone https://github.com/Remolino777/Propuesta-1
+Install the required dependencies:
+
+bash
+Copy code
+pip install -r requirements.txt
+Run the Streamlit application:
+
+bash
+Copy code
+streamlit run main.py
+Contributing
+Contributions are welcome! If you have any suggestions or ideas for improvement, feel free to open an issue or submit a pull request.
+
+License
+This project is licensed under the MIT License.
